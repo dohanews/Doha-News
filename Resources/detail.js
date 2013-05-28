c51ca80829439f4aa609b4b6b029ccfaca3ca5c0
Anas Halbawi <anashalb@gmail.com>
2013-05-28 16:51:40 +0300

    work in progress

--- Resources/detail.js
+++ Resources/detail.js
@@ -13,14 +13,39 @@ var localWebview = Titanium.UI.createWebView({
 	top:0,
     left:10,
     right:10,
-    height:1280,
-    width:720,
+    height:Ti.UI.currentWindow.getHeight,
+    width:Ti.UI.currentWindow.getWidth,
     backgroundColor:'transparent',
-    touchEnabled:false,
 	scalesPageToFit: true,
 	html:content,
 	enableZoomControls: false,
 });
 
+var incText = Titanium.UI.createButton({
+	title:'+',
+	left:500,
+	top:500});
 
+incText.addEventListener('click',function(e){
+	alert('incPressed');
+	localWebview.evalJS(
+		'var p = document.getElementsByTagName(\'p\');for(i=0;i<p.length;i++) { p[i].style.fontSize = 20"px"};'
+	);
+});
+
+var decText = Titanium.UI.createButton({
+	title:'-',
+	left:600,
+	top:600});
+	
+decText.addEventListener('click',function(e){
+	alert('decPressed');
+	localWebview.evalJS(
+		'var p = document.getElementsByTagName(\'p\');for(i=0;i<p.length;i++) { p[i].style.fontSize = 12"px"};'
+	);
+});
+
+alert('opened');
 win.add(localWebview);
+win.add(decText);
+win.add(incText);
