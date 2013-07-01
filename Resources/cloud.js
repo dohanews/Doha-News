e974641b70988bb0c80b8b87897b014b77693949
Ali Naqi <anaqi@qatar.cmu.edu>
2013-07-01 16:29:21 +0300

    Submissions Upload Finished (photo/description)

--- Resources/cloud.js
+++ Resources/cloud.js
@@ -1,40 +1,42 @@
 var Cloud = require('ti.cloud');
 
 var win1 = Ti.UI.currentWindow;
-
+var height = Ti.Platform.displayCaps.platformHeight - 100
 
 
 //login as the cloud user....
 function init(_args) {
-if (!Cloud.sessionId) {
-Cloud.Users.login({
-login: 'doha',
-password: 'dohanews'
-}, function (e) {
-if (e.success) {
-// _args.success({user : e.users[0]});
-} else {
-_args.error({error: e.error});
-}
-});
-}
+	if (!Cloud.sessionId) {
+		Cloud.Users.login({
+		login: 'doha',
+		password: 'dohanews'
+	}, function (e) {
+		if (e.success) {
+			// _args.success({user : e.users[0]});
+		} else {
+			_args.error({error: e.error});
+		}
+	});
+	}
 };
 exports.init = init;
 
-function sendPiccy(_args) {
-// create a new photo
-Cloud.Photos.create({
-photo: Ti.Filesystem.getFile(Ti.Filesystem.
-applicationDataDirectory+'/'+_args),
-custom_fields: {Name: 'Ali Naqi'}
-}, function (e) {
-if (e.success) {
-var photo = e.photos[0];
-alert('Success:\n' +'id: ' + photo.id );
-} else {
-alert('Error:\\n' +
-((e.error && e.message) || JSON.stringify(e)));
-}
-});
+function sendPiccy(_args, name, email, description, location) {
+	// create a new photo
+	Cloud.Photos.create({
+		photo: Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory+'/'+_args),
+		custom_fields: {Name: name, Email: email, Caption: description, Location: location}
+		}, function (e) {
+			if (e.success) {
+				Cloud.onsendstream = function (e) {
+    			Ti.API.info('Please Wait, Uploading Photo '+(Math.floor(e.progress * 0.5*100)*2)+'% Complete');
+				};
+				var photo = e.photos[0];
+				alert('Photo Submitted' );
+				win1.close();
+			} else {
+				alert('Error:\\n' +((e.error && e.message) || JSON.stringify(e)));
+			}
+		});
 }
 exports.sendPiccy = sendPiccy;
