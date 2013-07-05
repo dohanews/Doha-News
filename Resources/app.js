090a832d0536b151f658283862bad1c4cd1eee62
Ali Naqi <anaqi@qatar.cmu.edu>
2013-07-05 14:36:44 +0300

    Photo Gallery

--- Resources/app.js
+++ Resources/app.js
@@ -20,6 +20,12 @@ var bookmarksWin = Titanium.UI.createWindow ({
    orientationModes: [Titanium.UI.PORTRAIT],
 });
 
+var photosWin = Titanium.UI.createWindow({
+	url: 'photos.js',
+	id: 'photos',
+	navBarHidden: true,
+})
+
 if (Ti.Platform.osname != 'android'){
 	bookmarksWin.navBarHidden = true;
 	articlesWin.navBarHidden = true;
@@ -37,16 +43,16 @@ var articles = Titanium.UI.createTab({
 	window: articlesWin,
 
 });
-// var photos = Titanium.UI.createTab({
-	// title: 'Photos',
-	// window: videosWin,
-// });
+var photos = Titanium.UI.createTab({
+	title: 'Photos',
+	window: photosWin,
+});
 
 //mainWin.open();
 Ti.App.tabgroup.addTab(articles);
 //Ti.App.tabgroup.addTab(book);
 Ti.App.tabgroup.addTab(bookmarks);
-//Ti.App.tabgroup.addTab(photos);
+Ti.App.tabgroup.addTab(photos);
 Ti.App.tabgroup.open();
 Ti.App.tabgroupVisible = true;
 
