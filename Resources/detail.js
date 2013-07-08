3f30eba0a5c6e69ffb7896fdd48139ef9fde3f58
Anas Halbawi <anashalb@gmail.com>
2013-07-08 17:20:20 +0300

    added sharing and bookmarking to article view (iOS)

--- Resources/detail.js
+++ Resources/detail.js
@@ -1,5 +1,8 @@
 var osname = Ti.Platform.osname;
+Ti.include('ios-sharing.js');////////////////
 var common = require('ios-common');
+var db = require('database');
+
 var fb_regex = /(^http:\/\/disqus.com\/_ax\/facebook\/complete)/;
 var twitter_regex = /(^http:\/\/disqus.com\/_ax\/twitter\/complete)/;
 var google_regex = /(^http:\/\/disqus.com\/_ax\/google\/complete)/;
@@ -7,23 +10,36 @@ var disqus_reqex = /(^http:\/\/disqus.com\/next\/login-success)/
 
 var win = Ti.UI.currentWindow;
 win.navBarHidden = true;
-win.layout = 'vertical';
 
 var content = win.content;
-
+var articleId = win.id; /////////////////
+var url = win.articleUrl;/////////////////
+var title = win.articleTitle;/////////////////
+var thumbnail = win.thumbnail;/////////////////
+var date = win.date;/////////////////
+var author =  win.author;/////////////////
 
 
 var header = common.create_header();
+
+
+var bookmark = create_bookmarks(title, url, author, content, date, articleId);
+bookmark.center = null;
+bookmark.right = 0;
+bookmark.top = '0dp';
+bookmark.zIndex = 50;
+header.add(bookmark);
 header.addEventListener('click',function(){
 	win.close();
 })
+
 var localWebview = Titanium.UI.createWebView({
-	//top:'.75cm',
+	top:'45dp',
     backgroundColor:'transparent',
 	html:content,
 	enableZoomControls: false,
 	textSize: 1,
-	disableBounce: true
+	disableBounce: true,
 });
 
 var create_disqus = function(){
@@ -55,7 +71,6 @@ var create_disqus = function(){
 var disqus = create_disqus();
 
 var textsize = Titanium.UI.createImageView({
-	title:'o',
 	right:'55dp',
 	image: 'images/textsize.png',
 	zIndex: 2,
@@ -79,7 +94,28 @@ textsize.addEventListener('click',function(e){
 	}
 });
 
+var share = Titanium.UI.createImageView({
+	left:'0',
+	image: 'images/search.png',
+	zIndex: 2,
+	height: '45dp',
+	width: '45dp',
+	bubbleParent: false,
+});
+
+share.addEventListener('click', function(){
+	sharekit.share({
+	  		title: title,
+			link: url,
+	    	view: textsize,
+	  });
+});
+
+header.add(share);
 header.add(textsize);
+
 win.add(header);
 win.add(localWebview);
+
+
 //win.add(disqus);
