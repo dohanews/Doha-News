ce69d8b0c39db8a78745a55fef0caa4e96757fb5
Anas Halbawi <anashalb@gmail.com>
2013-07-10 15:08:09 +0300

    Sharing, bookmarking from view

--- Resources/detail.js
+++ Resources/detail.js
@@ -1,36 +1,40 @@
 var osname = Ti.Platform.osname;
-Ti.include('ios-sharing.js');////////////////
-var common = require('ios-common');
 var db = require('database');
-
-var fb_regex = /(^http:\/\/disqus.com\/_ax\/facebook\/complete)/;
-var twitter_regex = /(^http:\/\/disqus.com\/_ax\/twitter\/complete)/;
-var google_regex = /(^http:\/\/disqus.com\/_ax\/google\/complete)/;
-var disqus_reqex = /(^http:\/\/disqus.com\/next\/login-success)/
 
 var win = Ti.UI.currentWindow;
 win.navBarHidden = true;
 
+var header;
+var common;
+
+if (osname != 'android'){
+	Ti.include('ios-sharing.js');
+	common = require('ios-common');
+	header = common.create_header();
+}
+else{
+	Ti.include('and-sharing.js');
+	Ti.include('and-common.js');
+	header = create_header();
+
+}
+	
 var content = win.content;
-var articleId = win.id; /////////////////
-var url = win.articleUrl;/////////////////
-var title = win.articleTitle;/////////////////
-var thumbnail = win.thumbnail;/////////////////
-var date = win.date;/////////////////
-var author =  win.author;/////////////////
-
-
-var header = common.create_header();
-
+var articleId = win.id;
+var url = win.articleUrl;
+var title = win.articleTitle;
+var thumbnail = win.thumbnail;
+var date = win.date;
+var author =  win.author;
 
 var bookmark = create_bookmarks(title, url, author, content, date, articleId);
-bookmark.center = null;
-bookmark.right = 0;
-bookmark.top = '0dp';
-bookmark.zIndex = 50;
-header.add(bookmark);
-header.addEventListener('click',function(){
-	win.close();
+	bookmark.center = null;
+	bookmark.right = 0;
+	bookmark.top = '0dp';
+	bookmark.zIndex = 50;
+	header.add(bookmark);
+	header.addEventListener('click',function(){
+		win.close();
 })
 
 var localWebview = Titanium.UI.createWebView({
@@ -103,57 +107,73 @@ var share = Titanium.UI.createImageView({
 	bubbleParent: false,
 });
 
-share.addEventListener('click', function(){
+if (osname == 'android'){
 	
-	var dialog = Ti.UI.createOptionDialog({
-		title: "Share",
-		options: ['Twitter', 'Facebook', 'Mail', 'Cancel'],
-		cancel: 3 // index of cancel option
-	});
- 
-	dialog.addEventListener('click', function(e) {
-		if(e.index == e.cancel) { 
-			return; 
-		}
+	share.addEventListener('click',function(){      
+		var activity = Ti.Android.currentActivity;
+		var intent = Ti.Android.createIntent({
+		            action: Ti.Android.ACTION_SEND,
+		            type: 'text/plain'
+		        });
 		
-		var sharer = null;
-		var shareTitle;
-
-		switch(e.index) {
-			case 0:
-				sharer = 'Twitter';
-				shareTitle = '@dohanews ' + title;
-				break;
-			case 1:
-				sharer = 'Facebook';
-				shareTitle = title;
-				break;
-			case 2:
-				sharer = 'Mail';
-				shareTitle = title;
-				break;
-		}
-	 
-		if (sharer == 'Mail'){
-			var emailDialog = Ti.UI.createEmailDialog({
-				subject: shareTitle,
-				messageBody: url,
-			});
-			emailDialog.open();
-		}
-		else{
-			sharekit.share({
-				title: shareTitle,
-				link: url,
-				sharer: sharer,
-				view: share
-			});
-		}
+		intent.putExtra(Ti.Android.EXTRA_TEXT, url);
+		intent.putExtra(Ti.Android.EXTRA_DONT_KILL_APP);
+		intent.putExtra(Ti.Android.EXTRA_SUBJECT, title);
+		activity.startActivity(Ti.Android.createIntentChooser(intent,'Share via'));	
 	});
+}
+else{
+	share.addEventListener('click', function(){
+		
+		var dialog = Ti.UI.createOptionDialog({
+			title: "Share",
+			options: ['Twitter', 'Facebook', 'Mail', 'Cancel'],
+			cancel: 3 // index of cancel option
+		});
+	 
+		dialog.addEventListener('click', function(e) {
+			if(e.index == e.cancel) { 
+				return; 
+			}
+			
+			var sharer = null;
+			var shareTitle;
 	
-	dialog.show();
-});
-
+			switch(e.index) {
+				case 0:
+					sharer = 'Twitter';
+					shareTitle = '@dohanews ' + title;
+					break;
+				case 1:
+					sharer = 'Facebook';
+					shareTitle = title;
+					break;
+				case 2:
+					sharer = 'Mail';
+					shareTitle = title;
+					break;
+			}
+		 
+			if (sharer == 'Mail'){
+				var emailDialog = Ti.UI.createEmailDialog({
+					subject: shareTitle,
+					messageBody: url,
+				});
+				emailDialog.open();
+			}
+			else{
+				sharekit.share({
+					title: shareTitle,
+					link: url,
+					sharer: sharer,
+					view: share
+				});
+			}
+		});
+		
+		dialog.show();
+	});
+}
 header.add(share);
 header.add(textsize);
 
