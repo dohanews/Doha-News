57606245a6dfb52f7d7a18720d96d3266ecac808
Anas Halbawi <anashalb@gmail.com>
2013-07-19 15:30:03 +0300

    content refresh

--- Resources/and-refresh.js
+++ Resources/and-refresh.js
@@ -65,7 +65,7 @@ var refresh = function(e){
 	
 	var loader = Titanium.Network.createHTTPClient();
 
-	loader.open("GET","http://dev.dohanews.co/api/adjacent/get_next_posts/?dev=1&id="+parseInt(recentID,10));
+	loader.open("GET","http://dndev.staging.wpengine.com/api/adjacent/get_next_posts/?id="+parseInt(recentID,10));
 	
 	loader.onload = function() 
 	{
@@ -75,24 +75,24 @@ var refresh = function(e){
 
 		if (wp_length > 0){
 			recentID = wordpress.posts[wp_length-1].id;
-			tbl.deleteRow(tableHeaderRow);
 		}
 		else{
 			actInd.hide();
 			labelStatus.text = 'That\'s all, folks!';
 			setTimeout(function(){
 				tbl.deleteRow(tableHeaderRow);
-				refreshing = false;
 			}, 2000);
 		}
-
+		alert(wp_length);
 		for (i = 0; i < wp_length; i++)
 		{
+			tbl.deleteRow(tableHeaderRow);
+			
 			if (firstAd == 0) {
 				var adMobRow = createAdMobView();
 				tbl.insertRowBefore(0, adMobRow);
 				firstAd = 10;
-			}
+			}c
 		
 			firstAd--;
 			var articleContent = wordpress.posts[i].content; // The tweet message
@@ -101,24 +101,32 @@ var refresh = function(e){
 			var id = wordpress.posts[i].id;
 			var url = wordpress.posts[i].url;
 			var date = wordpress.posts[i].date;
-			
+			var modified = wordpress.posts[i].modified;
 			var thumbail;
 
 			if (wordpress.posts[i].attachments.length > 0)
 				thumbnail = wordpress.posts[i].attachments[0].images.small.url
 			else 
 				thumbnail = 'images/default_thumb.png';
-
+			
 			// Create a row and set its height to auto
-			var articleRow = make_content_view(articleTitle, articleContent, thumbnail, url, id, date, author);
+			alert(date);
+			var articleRow = make_content_view(articleTitle, articleContent, thumbnail, url, id, date, author, modified);
+			table_rows[id] = articleRow;
+			
 			tbl.insertRowBefore(0, articleRow);
 		}
 		
 		articleData = tbl.data;
+		update_content();
+		refreshing = false;
 	}
 	
 	loader.onerror = function(e){
 		tbl.deleteRow(tableHeaderRow);
+		//change_date_labels();
+		update_content();
+		refreshing = false;
 	}
 	
 	loader.send();
@@ -127,16 +135,48 @@ var refresh = function(e){
     // now we're done; reset the loadData flag and start the interval up again
 };
 
-var change_date_labels = function(tbl){
-
-	for (i = 0; i < tbl.data[0].rows.length; i++){
-		if(tbl.data[0].rows[i].className == 'article'){
-			console.log((tbl.data[0].rows[i].date));
-			date = get_relative_time(tbl.data[0].rows[i].date);
-			tbl.data[0].rows[i].date_label.text = date;
-		}
+var change_date_labels = function(){
+	refreshing = true;
+	for (id in table_rows){
+		date = get_relative_time(table_rows[id].date);
+		table_rows[id].date_label.text = date;
 	}
+	refreshing = false;
 };
+
+var update_content = function(){
+	var rows = {};
+	for (id in table_rows){
+		rows[id] = (table_rows[id].modified);
+	}
+	
+	var json_rows = JSON.stringify(rows);
+	
+	var loader = Ti.Network.createHTTPClient({Timeout: 10000});
+	loader.open('GET','http://dndev.staging.wpengine.com/api/refresh/check_date_modified/?outdated_posts=' + json_rows);
+	
+	loader.onload = function(){
+		var modified_posts = JSON.parse(this.responseText);
+		alert(modified_posts.posts.length);
+		for (i = 0; i < modified_posts.posts.length; i++){
+			var id = modified_posts.posts[i].id;
+			
+			table_rows[id].title_label.text = modified_posts.posts[i].title;
+			table_rows[id].url = modified_posts.posts[i].url;
+			table_rows[id].content = modified_posts.posts[i].content;
+			table_rows[id].articleTitle = modified_posts.posts[i].title;
+			table_rows[id].id = modified_posts.posts[i].id;
+			table_rows[id].date = modified_posts.posts[i].date;
+			table_rows[id].modified = modified_posts.posts[i].modified;
+			alert(table_rows[id].title + ' ' + table_rows[id].date);
+		}	
+	}
+	
+	loader.onerror = function(){
+	}
+	
+	loader.send();
+}
 
 var refreshButton = Ti.UI.createButton({
 	title: 'R',
@@ -145,8 +185,9 @@ var refreshButton = Ti.UI.createButton({
 });
 
 refreshButton.addEventListener('click', function(){
+	change_date_labels();
 	refresh();
-	change_date_labels()
+	//update_content();
 });
 
 header.add(refreshButton);
