ff57f4534819a1beaafef70a630b1cd30e80b9e8
Anas Halbawi <anashalb@gmail.com>
2013-07-22 16:36:26 +0300

    load content on demand, and store in cachee

--- Resources/detail.js
+++ Resources/detail.js
@@ -1,33 +1,70 @@
 var osname = Ti.Platform.osname;
 var db = require('database');
+var cache = require('cache');
 
 var win = Ti.UI.currentWindow;
 win.navBarHidden = true;
 
 var header;
 var common;
-
+var activityIndicator;
 if (osname != 'android'){
 	Ti.include('ios-sharing.js');
 	common = require('ios-common');
 	header = common.create_header();
+	activityIndicator = Ti.UI.createActivityIndicator({
+		style: Ti.UI.iPhone.ActivityIndicatorStyle.DARK,
+		height:Ti.UI.SIZE,
+		width:Ti.UI.SIZE
+	});
+	
 }
 else{
 	Ti.include('and-sharing.js');
 	Ti.include('and-common.js');
 	header = create_header();
-
+	activityIndicator = Ti.UI.createActivityIndicator({
+		style: Ti.UI.ActivityIndicatorStyle.DARK,
+		height:Ti.UI.SIZE,
+		width:Ti.UI.SIZE
+	});
+	win.activity.onCreateOptionsMenu = function(e) {
+		var menu = e.menu;
+		var searchItem = menu.add({
+			title: 'Search',
+			itemId: 1,
+			icon: 'images/search.png',
+			showAsAction: Ti.Android.SHOW_AS_ACTION_ALWAYS, 
+		});
+	}
 }
-	
-var content = win.content;
+
+win.add(activityIndicator);
+activityIndicator.show();
+
+//var content = win.content;
 var articleId = win.id;
 var url = win.articleUrl;
 var title = win.articleTitle;
 var thumbnail = win.thumbnail;
 var date = win.date;
 var author =  win.author;
+var content;
 
-var bookmark = create_bookmarks(title, url, author, content, date, articleId);
+
+var localWebview = Titanium.UI.createWebView({
+		top:'45dp',
+	    backgroundColor:'transparent',
+		enableZoomControls: false,
+		textSize: 1,
+		disableBounce: true,
+});
+
+
+win.add(header);
+
+var loadDetail = function(){
+	var bookmark = create_bookmarks(title, url, author, content, date, articleId);
 	bookmark.center = null;
 	bookmark.right = 0;
 	bookmark.top = '0dp';
@@ -35,150 +72,135 @@ var bookmark = create_bookmarks(title, url, author, content, date, articleId);
 	header.add(bookmark);
 	header.addEventListener('click',function(){
 		win.close();
-})
-
-var localWebview = Titanium.UI.createWebView({
-	top:'45dp',
-    backgroundColor:'transparent',
-	html:content,
-	enableZoomControls: false,
-	textSize: 1,
-	disableBounce: true,
-});
-
-var create_disqus = function(){
-	var disqus = Titanium.UI.createWebView({
-		//top:'.75cm',
-	    //left:10,
-	    //right:10, 
-	    backgroundColor:'transparent',
-		enableZoomControls: false,
-		textSize: 1,
-		disableBounce: true,
-		url: "http://dev.dohanews.co/wp-content/public/disqusTest.html",
-		zIndex: 40,
+	})
+	
+	var textsize = Titanium.UI.createImageView({
+		right:'55dp',
+		image: 'images/textsize.png',
+		zIndex: 2,
+		height: '45dp',
+		width: '45dp',
+		bubbleParent: false,
+	});
+		
+	textsize.addEventListener('click',function(e){
+		if (localWebview.textSize == 0){
+			localWebview.evalJS('med();');
+			localWebview.textSize = 1;
+		}
+		else if (localWebview.textSize == 1){
+			localWebview.evalJS('inc();');
+			localWebview.textSize = 2;
+		}
+		else if (localWebview.textSize == 2){
+			localWebview.evalJS('dec();');
+			localWebview.textSize = 0;
+		}
 	});
 	
-	disqus.addEventListener('load',function(e){
-	   	console.log('Url is: '+e.url);
-		if(e.url.match(fb_regex) || e.url.match(twitter_regex) || e.url.match(google_regex) || e.url.match(disqus_reqex)){
-	    	console.log ('it\'s facebook');
-	    	win.remove(disqus);
-	    	disqus = null;
-	       	disqus = create_disqus();
-	       	win.add(disqus);
-    	}
+	var share = Titanium.UI.createImageView({
+		left:'0',
+		image: 'images/search.png',
+		zIndex: 2,
+		height: '45dp',
+		width: '45dp',
+		bubbleParent: false,
 	});
-	return disqus;
+	
+	if (osname == 'android'){
+		
+		share.addEventListener('click',function(){      
+			var activity = Ti.Android.currentActivity;
+			var intent = Ti.Android.createIntent({
+			            action: Ti.Android.ACTION_SEND,
+			            type: 'text/plain'
+			        });
+			
+			intent.putExtra(Ti.Android.EXTRA_TEXT, url);
+			intent.putExtra(Ti.Android.EXTRA_DONT_KILL_APP);
+			intent.putExtra(Ti.Android.EXTRA_SUBJECT, title);
+			activity.startActivity(Ti.Android.createIntentChooser(intent,'Share via'));	
+		});
+	}
+	else{
+		share.addEventListener('click', function(){
+			
+			var dialog = Ti.UI.createOptionDialog({
+				title: "Share",
+				options: ['Twitter', 'Facebook', 'Mail', 'Cancel'],
+				cancel: 3 // index of cancel option
+			});
+		 
+			dialog.addEventListener('click', function(e) {
+				if(e.index == e.cancel) { 
+					return; 
+				}
+				
+				var sharer = null;
+				var shareTitle;
+		
+				switch(e.index) {
+					case 0:
+						sharer = 'Twitter';
+						shareTitle = '@dohanews ' + title;
+						break;
+					case 1:
+						sharer = 'Facebook';
+						shareTitle = title;
+						break;
+					case 2:
+						sharer = 'Mail';
+						shareTitle = title;
+						break;
+				}
+			 
+				if (sharer == 'Mail'){
+					var emailDialog = Ti.UI.createEmailDialog({
+						subject: shareTitle,
+						messageBody: url,
+					});
+					emailDialog.open();
+				}
+				else{
+					sharekit.share({
+						title: shareTitle,
+						link: url,
+						sharer: sharer,
+						view: share
+					});
+				}
+			});
+			
+			dialog.show();
+		});
+	}
+	header.add(share);
+	header.add(textsize);
 }
 
-var disqus = create_disqus();
-
-var textsize = Titanium.UI.createImageView({
-	right:'55dp',
-	image: 'images/textsize.png',
-	zIndex: 2,
-	height: '45dp',
-	width: '45dp',
-	bubbleParent: false,
-});
-	
-textsize.addEventListener('click',function(e){
-	if (localWebview.textSize == 0){
-		localWebview.evalJS('med();');
-		localWebview.textSize = 1;
-	}
-	else if (localWebview.textSize == 1){
-		localWebview.evalJS('inc();');
-		localWebview.textSize = 2;
-	}
-	else if (localWebview.textSize == 2){
-		localWebview.evalJS('dec();');
-		localWebview.textSize = 0;
-	}
-});
-
-var share = Titanium.UI.createImageView({
-	left:'0',
-	image: 'images/search.png',
-	zIndex: 2,
-	height: '45dp',
-	width: '45dp',
-	bubbleParent: false,
-});
-
-if (osname == 'android'){
-	
-	share.addEventListener('click',function(){      
-		var activity = Ti.Android.currentActivity;
-		var intent = Ti.Android.createIntent({
-		            action: Ti.Android.ACTION_SEND,
-		            type: 'text/plain'
-		        });
-		
-		intent.putExtra(Ti.Android.EXTRA_TEXT, url);
-		intent.putExtra(Ti.Android.EXTRA_DONT_KILL_APP);
-		intent.putExtra(Ti.Android.EXTRA_SUBJECT, title);
-		activity.startActivity(Ti.Android.createIntentChooser(intent,'Share via'));	
-	});
+if (cache.exists(articleId)){
+	content = cache.getContent(articleId);
+	loadDetail();
+	localWebview.html = content;
+	activityIndicator.hide();
+	win.add(localWebview);
 }
 else{
-	share.addEventListener('click', function(){
-		
-		var dialog = Ti.UI.createOptionDialog({
-			title: "Share",
-			options: ['Twitter', 'Facebook', 'Mail', 'Cancel'],
-			cancel: 3 // index of cancel option
-		});
-	 
-		dialog.addEventListener('click', function(e) {
-			if(e.index == e.cancel) { 
-				return; 
-			}
-			
-			var sharer = null;
-			var shareTitle;
-	
-			switch(e.index) {
-				case 0:
-					sharer = 'Twitter';
-					shareTitle = '@dohanews ' + title;
-					break;
-				case 1:
-					sharer = 'Facebook';
-					shareTitle = title;
-					break;
-				case 2:
-					sharer = 'Mail';
-					shareTitle = title;
-					break;
-			}
-		 
-			if (sharer == 'Mail'){
-				var emailDialog = Ti.UI.createEmailDialog({
-					subject: shareTitle,
-					messageBody: url,
-				});
-				emailDialog.open();
-			}
-			else{
-				sharekit.share({
-					title: shareTitle,
-					link: url,
-					sharer: sharer,
-					view: share
-				});
-			}
-		});
-		
-		dialog.show();
-	});
+	var loader = Ti.Network.createHTTPClient({Timeout:1000});
+	loader.open('GET', 'http://dndev.staging.wpengine.com/?json=get_post&post_id=' + articleId + '&dev=1&include=content');
+	loader.onload = function(){		
+		var response = JSON.parse(this.responseText);
+		content = response.post.content;
+		cache.insert(articleId, content, date);
+		loadDetail();
+		localWebview.html = content;
+		activityIndicator.hide();
+		win.add(localWebview);
+
+	}
+	loader.onerror = function(){
+		alert('error');
+	}
+	loader.send();
 }
-header.add(share);
-header.add(textsize);
-
-win.add(header);
-win.add(localWebview);
-
-
 //win.add(disqus);
