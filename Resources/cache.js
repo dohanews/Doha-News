7ac71289b7687516f76245077a56de24204ba8a2
Anas Halbawi <anashalb@gmail.com>
2013-07-22 16:35:57 +0300

    cache module

--- Resources/cache.js
+++ Resources/cache.js
@@ -39,6 +39,18 @@ exports.getAll = function() {
 	return results;
 };
 
+exports.getContent = function(id){
+	var db = Ti.Database.open(DATABASE_NAME); 
+	var resultSet = db.execute('SELECT * FROM articles WHERE id=(?)', id);
+	var content;
+	
+	if (resultSet.isValidRow())
+		content = resultSet.fieldByName('content');
+	resultSet.close();
+	db.close();
+	return content;	
+}
+
 exports.dropTable = function(){
 	var db = Ti.Database.open(DATABASE_NAME); 
     db.execute('DROP TABLE articles');
@@ -55,7 +67,6 @@ exports.deleteId = function(id){
 	var db = Ti.Database.open(DATABASE_NAME);
     db.execute('DELETE FROM articles WHERE id=(?)', id);
     db.close();
-	
 };
 
 exports.remove = function(id){
@@ -67,10 +78,28 @@ exports.remove = function(id){
 };
 
 exports.insert = function(id, content, date){
-	var timestamp = Math.round(new Date().getTime() / 1000)
+	var timestamp = Math.round(new Date().getTime() / 1000);
 	var db = Ti.Database.open(DATABASE_NAME);
+	var resultSet = db.execute('SELECT COUNT(*) FROM articles');
+	var count;
+	if (resultSet.isValidRow()){
+		count = resultSet.field(0);	
+	}
+	if (count >= 30){		
+		var resultSet = db.execute('SELECT id FROM articles ORDER BY timestamp LIMIT 1');
+		var id_delete;
+		if (resultSet.isValidRow()){
+			id_delete = resultSet.field(0);
+
+			db.execute('DELETE FROM articles where id = ?', id_delete);
+			var resultSet = db.execute('SELECT id FROM articles WHERE id = ?', id_delete);
+
+		}
+	}
+
 	db.execute('INSERT INTO articles (id, content, date, timestamp) VALUES(?,?,?,?)',
-     		id, contentl, date, timestamp);
+     		id, content, date, timestamp);
+     
     var lastInsertRowId = db.lastInsertRowId;
     db.close();
 	return lastInsertRowId;
