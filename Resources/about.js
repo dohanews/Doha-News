94825a3cee9a932f437c36092c1b3849264da432
Anas Halbawi <anashalb@gmail.com>
2013-09-30 12:44:28 +0400

    ios

--- Resources/about.js
+++ Resources/about.js
@@ -3,11 +3,14 @@ win.layout = 'vertical';
 win.backgroundColor = 'white';
 
 var common;
+var font, boldFont = 'droidsans';
 
 if (Ti.Platform.osname != 'android'){
 	common = require('ios-common');
 	var header = common.create_header(true);
 	win.add(header);
+	font = 'Helvetica';
+	boldFont = 'Helvetica-Bold';
 }
 
 
@@ -19,7 +22,7 @@ var aboutView = Ti.UI.createView({
 });
 
 var scrollView = Ti.UI.createScrollView({
-  contentWidth: 'auto',
+  contentWidth: Ti.Platform.displayCaps.platformWidth,
   contentHeight: 'auto',
   width: Ti.Platform.displayCaps.platformWidth,
   height: '100%',
@@ -28,13 +31,14 @@ var scrollView = Ti.UI.createScrollView({
 var logo = Ti.UI.createImageView({
 	image: "images/dohanews-full.png",
 	top: '10dp',
+	width: '60%',
 });
 
 var about = Ti.UI.createLabel({
 	text: "We are Qatar’s first digital news organization.\n\nSince 2009, Doha News has been bringing you breaking news and the latest on politics, business, culture and life in and around Qatar.\n\nWe’re a startup staffed by a small but versatile team of experienced journalists, who’ve worked for Al Jazeera English, the BBC and the Wall Street Journal. Our goal is to educate, inform, and stimulate positive change in society.\n\nBlending curation with original reporting, our stories are driven by our community, but professionally written and edited to meet the highest standards of journalism.\n\nTips, comments or suggestions? Email us at editor@dohanews.co",
 	font:{
 			fontSize: '17dp',
-			fontFamily: 'droidsans',
+			fontFamily: font,
 		},
 	top: '10dp',
 	left: '15dp',
@@ -64,7 +68,7 @@ var createMember = function(name, position){
 		text: name, 
 		font: {
 			fontSize: '17dp',
-			fontFamily: 'droidsans',
+			fontFamily: font,
 		},
 		textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
 		top: '5dp',
@@ -74,7 +78,7 @@ var createMember = function(name, position){
 		text: position,
 		font: {
 			fontSize: '15dp',
-			fontFamily: 'droidsans',
+			fontFamily: font,
 			//fontWeight: 'bold',
 		},
 		color: '#70193c',
@@ -93,7 +97,7 @@ var credits = Ti.UI.createLabel({
 	top: '10dp',
 	font: {
 			fontSize: '19dp',
-			fontFamily: 'droidsans',
+			fontFamily: boldFont,
 			fontWeight: 'bold',
 		},
 	textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
@@ -105,7 +109,7 @@ var appTeam = Ti.UI.createLabel({
 	top: '25dp',
 	font: {
 			fontSize: '17dp',
-			fontFamily: 'droidsans',
+			fontFamily: font,
 			fontWeight: 'bold',
 		},
 	textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
@@ -114,7 +118,7 @@ var appTeam = Ti.UI.createLabel({
 var anas = createMember('Anas Halbawi', 'Project Manager & Lead Develper');
 var ali = createMember('Ali Naqi', 'Product Manager & Developer');
 var noshin = createMember('Noshin Nisa', 'User Interface Lead');
-var zaid = createMember('Zaid Haque', 'User Interface Consultant');
+var zaid = createMember('Zaid Haque', 'User Experience Consultant');
 
 var comd = createMember('Check Out My Design', 'Design Consultants');
 comd.top = '10dp';
@@ -124,7 +128,7 @@ var coreTeam = Ti.UI.createLabel({
 	top: '15dp',
 	font: {
 			fontSize: '17dp',
-			fontFamily: 'droidsans',
+			fontFamily: boldFont,
 			fontWeight: 'bold',
 		},
 	textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
@@ -145,7 +149,7 @@ var version = Ti.UI.createLabel({
 	text: 'Version', 
 	font: {
 		fontSize: '17dp',
-		fontFamily: 'droidsans',
+		fontFamily: font,
 	},
 	textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
 });
@@ -154,7 +158,7 @@ var number = Ti.UI.createLabel({
 	text: '1.0.0',
 	font: {
 		fontSize: '17dp',
-		fontFamily: 'droidsans',
+		fontFamily: font,
 	},
 	textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
 });
